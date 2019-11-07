eeb3699b1cfb47e63199628e9b54ec3dcbdec695
fmmachado10 <fabricio.machado@tcm.go.gov.br>
2019-11-07 15:03:08 -0300

    Adicionando correções

--- src/main/java/com/projetoSpringBoot/controlador/FotosUploadController.java
+++ src/main/java/com/projetoSpringBoot/controlador/FotosUploadController.java
@@ -2,12 +2,15 @@ package com.projetoSpringBoot.controlador;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.projetoSpringBoot.storage.Arquivos;
+
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/fotos")
@@ -16,6 +19,8 @@ public class FotosUploadController {
 	@Autowired
 	private Arquivos arq;
 	
+	@PostMapping
+	@ApiOperation(value = "Upload de foto.")
 	public void upload(@RequestParam MultipartFile foto) {
 		
 		arq.salvarFoto(foto);
